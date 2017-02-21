db8acb39ba3e686583068080cde5c59c436b0300
Suneet Tipirneni <suneettipirneni@gmail.com>
2017-02-21 14:44:04 -0500

    Added table view with cells

---
+++ Conference Plus/PeopleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleTableViewCell.swift
+//  Conference Plus
+//
+//  Created by Suneet Tipirneni on 2/21/17.
+//  Copyright © 2017 Suneet Tipirneni. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Conference Plus/PeopleViewController.swift
+++ Conference Plus/PeopleViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class PeopleViewController: UIViewController {
+class PeopleViewController: UIViewController, UITableViewDataSource {
+    
+    // MARK: - Class Variables
+    
+    var peopleTableview:UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Table View Setup
+        peopleTableview = UITableView(frame: CGRect())
+        peopleTableview.translatesAutoresizingMaskIntoConstraints = false
+        peopleTableview.dataSource = self
+        peopleTableview.register(PeopleTableViewCell.self, forCellReuseIdentifier: "peopleCell")
+        
+        // Layout.
+        self.view.addSubview(peopleTableview)
+        peopleTableview.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        peopleTableview.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        peopleTableview.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        peopleTableview.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +38,19 @@ class PeopleViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 5
+    }
+    
+    
+  
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell") as! PeopleTableViewCell
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
